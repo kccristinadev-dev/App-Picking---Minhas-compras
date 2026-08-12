eef4a537d7d5d4af08fa618a02e6574253e47f4a
Cristina <kc.cristinadev@gmail.com>
2026-08-12 18:45:52 -0300

    Atualizar o ValidationHelper.cs

--- Helpers/ValidationHelper.cs
+++ Helpers/ValidationHelper.cs
@@ -3,41 +3,11 @@ using  Produtos.Models;
 
 namespace AppPickingMinhasCompras.Helpers;
 
-public static class ValidationHelper
-{
-    public static bool IsValidEmail(string email)
-    {
-        try
-        {
-            var addr = new System.Net.Mail.MailAddress(email);
-            return addr.Address == email;
-        }
-        catch
-        {
-            return false;
-        }
-    }
+public class SQLiteDataBaseHelpers{
+   
+readoly SQLiteConnection _connection;
 
-    public static bool IsValidPrice(string priceText)
-    {
-        return decimal.TryParse(priceText, out var price) && price > 0;
-    }
+public SQLiteDataBaseHelpers() {
+}
 
-    public static bool IsValidQuantity(string quantityText)
-    {
-        return int.TryParse(quantityText, out var quantity) && quantity > 0;
-    }
-
-    public static bool IsNotNullOrEmpty(string text)
-    {
-        return !string.IsNullOrWhiteSpace(text);
-    }
-
-    public static string TruncateText(string text, int maxLength)
-    {
-        if (string.IsNullOrEmpty(text) || text.Length <= maxLength)
-            return text;
-
-        return text.Substring(0, maxLength) + "...";
-    }
 }
