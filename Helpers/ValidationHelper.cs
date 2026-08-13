ce122a1b3559d1f1a61ac7b9672e5d34d0b1d0d7
Cristina <kc.cristinadev@gmail.com>
2026-08-13 16:36:18 -0300

    Atualizar o ValidationHelper.cs

--- Helpers/ValidationHelper.cs
+++ Helpers/ValidationHelper.cs
@@ -7,7 +7,19 @@ public class SQLiteDataBaseHelpers{
    
   readonly SQLiteConnection _connection;
 
-public SQLiteDataBaseHelpers() {
+public SQLiteDataBaseHelpers(string pach) {
+_conn = new SQLiteAsyncConnecton(pach);
+_conn CreateTableAsync<Produto>().waint();
+}
+public void Insert(Produto p){
+
 }
 
+public void Update(Produto p){
+
+}
+
+public void Delete(Produto p){
+
+}
 }
