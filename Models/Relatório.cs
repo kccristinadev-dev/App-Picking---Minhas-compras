6dfb4ced6f497ed4903fd633427bb7d1042d0f23
Cristina <kc.cristinadev@gmail.com>
2026-09-21 16:04:55 -0300

    Criar Relatório.cs

---
+++ Models/Relatório.cs
@@ -0,0 +1,8 @@
+namespace AppPickingMinhasCompras.Models;
+
+public class RelatorioCategoria
+{
+    public string NomeCategoria { get; set; } = string.Empty;
+
+    public decimal TotalGasto { get; set; }
+}
